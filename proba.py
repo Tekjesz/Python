00b9fef5dc746c81e0274e7ba1717083a3c4963f
Roland Pintér <pinter.roland.ms@gmail.com>
2022-11-02 18:25:10 +0100

    review finding fixed about type hints

--- proba.py
+++ proba.py
@@ -1,8 +1,10 @@
-def add(a, b):
+def add(a: int, b: int) -> int:
     return a+b
 
-def multiply(a, b):
+def multiply(a: int, b: int) -> int:
     return a*b
 
 if __name__ == "__main__":
     print(add(1, 2))
+    print(multiply(1, 2))
+    c = multiply(3, 4)
