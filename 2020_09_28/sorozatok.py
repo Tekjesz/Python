0213b12e1ac4480451e8c4b81f1ca0f07ebd9e99
Tekjeksz <ervinke0322@gmail.com>
2022-11-25 13:35:54 +0100

    task

---
+++ 2020_09_28/sorozatok.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+
+def store_txt(name):
+    sorted_data = []
+    with open(name, "r") as f:
+        lines = f.readlines
+
+        for i in range(0, len(lines)):
+            
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    store_txt("lista.txt")
